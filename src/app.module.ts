0afa0297b692c7ed1285f0264ac14898b8ca4cb8
Jacopo Mucci <mucci.jacopo@gmail.com>
2021-07-03 19:20:30 +0200

    initialize postgres connection with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsResolver } from './authors.resolver';
 
 @Module({
-  imports: [GraphQLModule.forRoot({
+  imports: [
+    GraphQLModule.forRoot({
     autoSchemaFile: true
-  })],
+    }), 
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      username: "admin",
+      password: "password1!",
+      database: "App",
+      entities: [],
+      synchronize: true
+    })
+],
   controllers: [AppController],
   providers: [AppService, AuthorsResolver],
 })
