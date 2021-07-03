72b89112f4213668ca4734d78de34a47649aecb9
Jacopo Mucci <mucci.jacopo@gmail.com>
2021-07-03 12:14:34 +0200

    initialize graphql server + first demo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorsResolver } from './authors.resolver';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: true
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthorsResolver],
 })
 export class AppModule {}
---
+++ src/authors.resolver.ts
@@ -0,0 +1,34 @@
+import { Args, Int, Query, Resolver } from "@nestjs/graphql";
+import { Author } from "./models/author.model";
+
+const authorsModel: Author[] = [{
+    id: 1,
+    firstName: "Jacopo"
+}]
+
+@Resolver(of => Author)
+export class AuthorsResolver {
+  constructor(
+  ) {}
+
+  @Query(() => Author)
+  async author(@Args('id', { type: () => Int }) id: number) {
+    return authorsModel.find(a => a.id == id);
+  }
+
+  @Query(() => [Author], { name: 'authors'})
+  async getAuthors() {
+    return authorsModel.slice()
+  }
+
+//   @Query(returns => [Author])
+//   async authors() {
+//       return [...authorsModel];
+//   }
+
+//   @ResolveField()
+//   async posts(@Parent() author: Author) {
+//     const { id } = author;
+//     return this.postsService.findAll({ authorId: id });
+//   }
+}
