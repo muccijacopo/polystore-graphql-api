190f29ec5dd29a09a582064f7f535a5b9e017308
Jacopo Mucci <mucci.jacopo@gmail.com>
2021-09-01 17:54:18 +0200

    feat: kafka is now working

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,58 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-import * as Redis from 'redis';
+import { Kafka } from 'kafkajs';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000, "0.0.0.0");
+
+  const kafka = new Kafka({
+    clientId: 'app',
+    brokers: ['localhost:9092'],
+    ssl: false
+  });
+
+  const producer = kafka.producer()
+  await producer.connect();
+
+  const consumer = kafka.consumer({ groupId: 'test-group' })
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'topic1', fromBeginning: true })
+
+  consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({ topic, value: message.value.toString() })
+    },
+  })
+
+
+  for (let i = 1; i <= 1000; i++) {
+    producer.send({
+      topic: "topic1",
+      messages: [{ value: `Hello from ${i}`}]
+    })
+  }
+
+
+
+  
+
+  // // Consuming
+  // const consumer = kafka.consumer({ groupId: 'test-group' })
+  // await consumer.connect()
+  // await consumer.subscribe({ topic: 'UploadFile', fromBeginning: true })
+
+  // await consumer.run({
+  //   eachMessage: async ({ topic, partition, message }) => {
+  //     console.log({
+  //       partition,
+  //       offset: message.offset,
+  //       value: message.value.toString(),
+  //     })
+  //   }
+  // });
 }
+
 bootstrap();
