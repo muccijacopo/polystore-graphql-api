9081adaf95c146610f94f9de884ea69f0bf816ae
Jacopo Mucci <mucci.jacopo@gmail.com>
2021-09-05 20:22:42 +0200

    CI: now Kafka works out of Docker scope and inside

--- ingestion/main.py
+++ ingestion/main.py
@@ -6,32 +6,37 @@ import ast
 
 def main():
     broker = KafkaService()
+    rows1 = 0
+    for df in pd.read_csv("artists.csv", chunksize=1000):
+        df: pd.DataFrame = df
+        rows1 += len(df.index)
+        # for _, row in df.iterrows():
+        #     data = {
+        #         'id': row['id'],
+        #         'name': row['name'],
+        #         'popularity': row['popularity'],
+        #         'followers': row['followers']
+        #     }
+            # broker.send_message(topic='artists', value=data)
+    print(f"Artists: {rows1}")
 
-    for df in pd.read_csv("artists.csv", chunksize=10):
+    rows2 = 0
+    for df in pd.read_csv("tracks.csv", chunksize=1000):
         df: pd.DataFrame = df
-        for _, row in df.iterrows():
-            data = {
-                'id': row['id'],
-                'name': row['name'],
-                'popularity': row['popularity'],
-                'followers': row['followers']
-            }
-            broker.send_message(topic='artists', value=data)
+        rows2 += len(df.index)
+        # for _, row in df.iterrows():
+        #     data = {
+        #         'id': row['id'],
+        #         'name': row['name'],
+        #         'popularity': row['popularity'],
+        #         'duration': row['duration_ms'],
+        #         'explicit': row['explicit'],
+        #         'release_date': row['release_date'],
+        #         'artist_id': ast.literal_eval(row['id_artists'])[0]
+        #     }
     
-    for df in pd.read_csv("tracks.csv", chunksize=10):
-        df: pd.DataFrame = df
-        for _, row in df.iterrows():
-            data = {
-                'id': row['id'],
-                'name': row['name'],
-                'popularity': row['popularity'],
-                'duration': row['duration_ms'],
-                'explicit': row['explicit'],
-                'release_date': row['release_date'],
-                'artist_id': ast.literal_eval(row['id_artists'])[0]
-            }
-    
-            broker.send_message(topic='tracks', value=data)
+            # broker.send_message(topic='tracks', value=data)
+    print(f"Tracks: {rows2}")
 
 if __name__ == '__main__':
     main()
