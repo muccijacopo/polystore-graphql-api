2bd5d65acce1d8b8ae3943f5c1ac9f287fd31b13
Jacopo Mucci <mucci.jacopo@gmail.com>
2021-09-05 17:24:29 +0200

    CI: updated docker compose for kafka service

--- api/src/kafka.service.ts
+++ api/src/kafka.service.ts
@@ -8,41 +8,29 @@ import { TrackService } from "./tracks/track.service";
 @Injectable()
 export class KafkaService {
   async onModuleInit() {
-    // const kafka = new Kafka({
-    //   clientId: "app",
-    //   brokers: ["localhost:9092"],
-    //   ssl: false,
-    // });
+    const kafka = new Kafka({
+      clientId: "app",
+      brokers: ["kafka:9092"],
+      ssl: false,
+    });
 
-    // const consumer = kafka.consumer({ groupId: "test-group" });
-    // await consumer.connect();
-    // await consumer.subscribe({ topic: "tracks", fromBeginning: false });
-    // await consumer.subscribe({ topic: 'artists', fromBeginning: false });
+    const consumer = kafka.consumer({ groupId: "test-group" });
+    await consumer.connect();
+    await consumer.subscribe({ topic: "tracks", fromBeginning: false });
+    await consumer.subscribe({ topic: 'artists', fromBeginning: false });
 
-    const tracksDataQueue: AddTrackInput[] = [];
-    const artistsDataQueue: AddArtistInput[] = [];
-
-    // consumer.run({
-    //   eachMessage: async ({ topic, partition, message }) => {
-    //     console.log({ topic, message: message.value.toString() })
-    //     if (topic == 'artists') {
-    //       const data = JSON.parse(message.value.toString()) as AddArtistInput;
-    //       await this.artistService.addArtist(data);
-    //     } else if (topic == 'tracks') {
-    //       const data = JSON.parse(message.value.toString()) as AddTrackInput;
-    //       await this.trackService.saveNewTrack(data);
-    //     }
-    //   },
-    // });
-
-    // setInterval(async () => {
-    //   const tracksDataQueueCopy = tracksDataQueue.slice();
-    //   if (tracksDataQueueCopy.length > 0) {
-    //     for (let el of tracksDataQueueCopy) {
-    //       await this.trackService.saveNewTrack(el);
-    //     }
-    //   }
-    // }, 1000)
+    consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({ topic, message: message.value.toString() })
+        if (topic == 'artists') {
+          const data = JSON.parse(message.value.toString()) as AddArtistInput;
+          await this.artistService.addArtist(data);
+        } else if (topic == 'tracks') {
+          const data = JSON.parse(message.value.toString()) as AddTrackInput;
+          await this.trackService.saveNewTrack(data);
+        }
+      },
+    });
   }
 
   constructor(private trackService: TrackService, private artistService: ArtistService) {}
